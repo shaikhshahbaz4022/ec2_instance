172e315dbf4912c6ebd55a0eba2717f93a702c99
unknown <shaikhshahbaz4022@gmail.com>
2023-09-25 23:01:05 +0530

    Updates

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express()
 app.use(cors())
 
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.status(200).send(`<h1>EC2 Launced Succesfully</h1>`)
+})
+app.get("/", (req, res) => {
+    res.status(200).send({ 'msg': "Success Route Hit " })
 })
 app.listen(8000, () => {
     console.log("Server is Connected to PORT", 8000);
