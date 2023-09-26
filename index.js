7435d65b359e8372b54d2e4683fae2ecf9567e18
unknown <shaikhshahbaz4022@gmail.com>
2023-09-26 22:08:16 +0530

    Updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(cors())
 
 
 app.get('/test', (req, res) => {
-    res.status(200).send(`<h1>EC2 Launced Succesfully</h1>`)
+    res.status(200).send({ "msg": "Running Fine" })
 })
 app.get("/", (req, res) => {
     res.status(200).send({ 'msg': "Success Route Hit " })
